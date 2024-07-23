77e9a99ef9a10dcd484ba8492bd484e4989d1aa1
DLOzTOR <dloztor.aar@outlook.com>
2024-07-23 22:07:53 +0300

    add linked list add at fnc

--- src/linked_list.c
+++ src/linked_list.c
@@ -16,6 +16,7 @@ int_linked_list_node *create_int_linked_list_node(int data)
 {
   int_linked_list_node *t = malloc(sizeof(int_linked_list_node));
   t->data = data;
+  t->next = NULL;
   return t;
 }
 
@@ -37,6 +38,42 @@ void int_linked_list_add(int_linked_list *list, int data)
     list->first = malloc(sizeof(int_linked_list_node));
     list->first->data = data;
     list->first->next = NULL;
+  }
+}
+
+int int_linked_list_add_at(int_linked_list *list, size_t position, int data) // -1 if can't add
+{
+  size_t current_none_index = 0;
+  int_linked_list_node *current_node = list->first;
+  if (position == 0)
+  {
+    int_linked_list_node *t_node = list->first;
+    list->first = create_int_linked_list_node(data);
+    list->first->next = t_node;
+    return 0;
+  }
+  else if (list->first == NULL)
+  {
+    return -1;
+  }
+  else
+  {
+    while (current_node->next != NULL && current_none_index < position)
+    {
+      current_none_index++;
+      current_node = current_node->next;
+    }
+    if (current_node->next == NULL)
+    {
+      return -1;
+    }
+    else
+    {
+      int_linked_list_node *t_node = current_node->next;
+      current_node->next = create_int_linked_list_node(data);
+      current_node->next->next = t_node;
+      return 0;
+    }
   }
 }
 
@@ -89,12 +126,14 @@ void linked_list_test()
 {
   int_linked_list int_list = {0};
   int_linked_list_add(&int_list, 0);
+  int_linked_list_add_at(&int_list, 0, -1);
   for (int i = 1; i < 10; i++)
   {
     int_linked_list_add(&int_list, i);
   }
   int_linked_list_node *current_node = int_list.first;
   int_linked_list_print(&int_list);
+  int_linked_list_add_at(&int_list, 2, -100);
   printf("list nodes count: %i.\n", int_linked_list_cout(&int_list));
   int_linked_list_remove_last(&int_list);
   printf("remove last\n");

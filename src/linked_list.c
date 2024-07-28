bb1b75906062ff4b8b3451cce4fc2ee85426e08b
DLOzTOR <dloztor.aar@outlook.com>
2024-07-28 14:27:20 +0300

    add linked list sort fnc

--- src/linked_list.c
+++ src/linked_list.c
@@ -178,6 +178,54 @@ int int_linked_list_cout(int_linked_list *list)
   return counter;
 }
 
+void int_linked_list_sort(int_linked_list *list)
+{
+  if (list->first == NULL)
+  {
+    return;
+  }
+  if (list->first->next == NULL)
+  {
+    return;
+  }
+  int is_sorted = 0;
+  int_linked_list_node *curren_node = list->first, *prev = NULL;
+  while (is_sorted == 0)
+  {
+    is_sorted = 1;
+    curren_node = list->first;
+    prev = list->first;
+    while (curren_node->next != NULL)
+    {
+      if (curren_node->data > curren_node->next->data)
+      {
+        is_sorted = 0;
+        if (curren_node == list->first)
+        {
+          list->first = curren_node->next;
+          curren_node->next = curren_node->next->next;
+          list->first->next = curren_node;
+          curren_node = list->first->next;
+          break;
+        }
+        else
+        {
+          prev->next = curren_node->next;
+          curren_node->next = curren_node->next->next;
+          prev->next->next = curren_node;
+          curren_node = prev->next->next;
+          break;
+        }
+      }
+      else
+      {
+        prev = curren_node;
+        curren_node = curren_node->next;
+      }
+    }
+  }
+}
+
 void int_linked_list_print(int_linked_list *list)
 {
   int_linked_list_node *current_node = list->first;
@@ -191,20 +239,22 @@ void int_linked_list_print(int_linked_list *list)
 void linked_list_test()
 {
   int_linked_list int_list = {0};
-  int_linked_list_add(&int_list, 0);
-  for (int i = 1; i < 10; i++)
+  for (int i = 10; i > 0; i--)
   {
     int_linked_list_add(&int_list, i);
   }
-  int_linked_list_get_at(&int_list, 4)->data = -4;
-  printf("4 element is: %i.\n", int_linked_list_get_at(&int_list, 4)->data);
-  int_linked_list_add_at(&int_list, 0, -1);
+  // int_linked_list_get_at(&int_list, 4)->data = -4;
+  // printf("4 element is: %i.\n", int_linked_list_get_at(&int_list, 4)->data);
+  // int_linked_list_add_at(&int_list, 0, -1);
   int_linked_list_print(&int_list);
-  int_linked_list_add_at(&int_list, 2, -100);
-  printf("list nodes count: %i.\n", int_linked_list_cout(&int_list));
-  int_linked_list_remove_last(&int_list, 1);
-  int_linked_list_remove_at(&int_list, 0, 1);
-  int_linked_list_remove_at(&int_list, 4, 1);
-  printf("list nodes count: %i.\n", int_linked_list_cout(&int_list));
+  // int_linked_list_add_at(&int_list, 2, -100);
+  // printf("list nodes count: %i.\n", int_linked_list_cout(&int_list));
+  // int_linked_list_remove_last(&int_list, 1);
+  // int_linked_list_remove_at(&int_list, 0, 1);
+  // int_linked_list_remove_at(&int_list, 4, 1);
+  // printf("list nodes count: %i.\n", int_linked_list_cout(&int_list));
+  // int_linked_list_print(&int_list);
+  int_linked_list_sort(&int_list);
+  printf("sorted\n");
   int_linked_list_print(&int_list);
 }
